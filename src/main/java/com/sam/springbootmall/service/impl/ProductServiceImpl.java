9d397e4ee3a3925f1af66658e6ffab4047a7b107
samping94 <samping0501@gmail.com>
2022-08-21 14:21:17 +0800

    商品功能 - 實作新增商品

--- src/main/java/com/sam/springbootmall/service/impl/ProductServiceImpl.java
+++ src/main/java/com/sam/springbootmall/service/impl/ProductServiceImpl.java
@@ -1,6 +1,7 @@
 package com.sam.springbootmall.service.impl;
 
 import com.sam.springbootmall.dao.ProductDao;
+import com.sam.springbootmall.dto.ProductRequest;
 import com.sam.springbootmall.model.Product;
 import com.sam.springbootmall.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,5 +17,10 @@ public class ProductServiceImpl implements ProductService {
     public Product getProductById(Integer productId) {
         Product productById = productDao.getProductById(productId);
         return productById;
+    }
+
+    @Override
+    public Integer createProduct(ProductRequest productRequest) {
+        return productDao.createProduct(productRequest);
     }
 }
